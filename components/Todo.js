a944bd47175524eb0b5fdc762aa7a1b3abf406ca
Yuldash Atajanov <cybergod1123@gmail.com>
2021-03-24 14:29:33 +0300

    серверная сторона выполнена

--- components/Todo.js
+++ components/Todo.js
@@ -10,7 +10,8 @@ const url = '/api/getuser';
 
 export default function Todo(props) {
     let [todo, setList] = useState([]); //список задач
-    let [showtype,setShow]=useState(""); //какой тип элементов отобразить (все/невыполенный)
+    let [showtype, setShow] = useState(""); //какой тип элементов отобразить (все/невыполенный)
+    let [blocking, setBlocking] = useState(false);
     const userId = getClientId(); //идентификатор клиента 
 
     //загрузка начальных данных с сервера
@@ -30,63 +31,98 @@ export default function Todo(props) {
         })
     }, []);
 
-    
+
 
     /*обработчки событий*/
 
     //добавить новую задачу
     const addTodo = (value) => {
-        let request = {
-            userid: userId,
-            action: "add",
-            content: value,
-            date: new Date()
+        if (blocking === false) {
+            setBlocking(true);
+            let request = {
+                userid: userId,
+                action: "add",
+                content: value,
+            }
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(request)
+            }).then(res => res.json()).then(json => {
+                let list = [...todo];
+                list.push({
+                    id: json.id,
+                    task: value,
+                    completed: false,
+                    date: json.date
+                })
+                setList(list);
+                setBlocking(false);
+            })
         }
-    
-        let list = [...todo];
-        list.push({
-            task: value,
-            completed: false,
-            date: request.date.toString()
-        })
-        setList(list);
     }
 
     //запрос для отметки выполненной задачи
     const onCompleted = (id) => {
-        if (todo[id].completed === false) {
-            let request = {
-                userid: userId,
-                action: "complete",
-                taskid: id
-            }
+        if (blocking === false) {
+            setBlocking(true);
+            if (todo[id].completed === false) {
+                let request = {
+                    userid: userId,
+                    action: "complete",
+                    taskid: todo[id].id
+                }
+                fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json;charset=utf-8'
+                    },
+                    body: JSON.stringify(request)
+                }).then(res => {
+                    if (res.status === 200)
+                        setBlocking(false);
+                })
 
-            let list = [...todo];
-            list[id].completed = true;
-            setList(list);
-            console.log("completed");
+                let list = [...todo];
+                list[id].completed = true;
+                setList(list);
+            }
         }
     }
     //запрос на удаления задачи
     const onRemove = (id) => {
-        let request = {
-            userid: userId,
-            action: "delete",
-            taskid: id
+        if (blocking === false) {
+            setBlocking(true);
+            let request = {
+                userid: userId,
+                action: "delete",
+                taskid: todo[id].id
+            }
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(request)
+            }).then(res => {
+                if (res.status === 200)
+                    setBlocking(false);
+            })
+            let list = [...todo];
+            list = list.filter((item, index) => {
+                return (index !== id) ? true : false;
+            })
+            setList(list);
         }
-
-        let list = [...todo];
-        list = list.filter((item, index) => {
-            return (index !== id) ? true : false;
-        })
-        setList(list);
     }
 
     // список задач помещается как дочерние элементы компонента listview
-    let filter=[...todo];
-    if(showtype==="uncompleted"){
-        filter=todo.filter((item,index)=>{
-            return (item.completed===false)?true:false
+    let filter = [...todo];
+    if (showtype === "uncompleted") {
+        filter = todo.filter((item, index) => {
+            return (item.completed === false) ? true : false
         })
     }
     let Items = filter.map((item, index) => {
@@ -99,16 +135,17 @@ export default function Todo(props) {
                 isCompleted={item.completed}
                 content={item.task}
                 date={item.date}
+
             />
         )
     })
-    
+
     //покзаать только не выполненные задачи
-    const showUncomp =(ev)=>{
+    const showUncomp = (ev) => {
         setShow("uncompleted");
     }
     //показать все задачи
-    const showAll=(ev)=>{
+    const showAll = (ev) => {
         setShow("all");
     }
 
@@ -118,7 +155,7 @@ export default function Todo(props) {
             <ListView>
                 {Items}
             </ListView>
-            <Footer todo={todo} showUncompleted={showUncomp} showAll={showAll}/>
+            <Footer todo={todo} showUncompleted={showUncomp} showAll={showAll} />
         </div>
     )
 }
@@ -132,6 +169,7 @@ function getList(json) {
     let list = [];
     for (let id in json) {
         list.push({
+            id: json[id].id,
             completed: json[id].completed,
             date: json[id].date,
             task: json[id].task
--- pages/api/getuser.js
+++ pages/api/getuser.js
@@ -19,6 +19,74 @@ export default (request, response) => {
                 sendFile(fullpath,response);
                 break;
             }
+            case "add":{
+
+                console.log("adding");
+                let date=new Date();
+                let item={
+                    completed: false,
+                    task: request.body.content,
+                    date: date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()
+                }
+                fs.readFile(fullpath,"utf8",(err,data)=>{
+                    if(err){
+                        console.log("error");
+                        return;
+                    }
+                    let json = JSON.parse(data);
+                    let lastindex=-1;
+                    for(let i in json){
+                        lastindex=parseInt(i);
+                    }
+                    item.id=lastindex+1;
+                    json[lastindex+1]=item;
+                    fs.writeFile(fullpath,JSON.stringify(json),(err)=>{
+                        if(err)
+                            console.log("error");
+                    });
+                    response.status(200).json({date: item.date,id:item.id});
+                })
+                break;
+            }
+            case "complete":{
+                console.log("copleted");
+                fs.readFile(fullpath,"utf8",(err,data)=>{
+
+                    if(err) {
+                        console.log(err);
+                        return;
+                    }
+                    let json = JSON.parse(data);
+                    let index = findIndex(json,request.body.taskid);
+                    
+                    json[index].completed=true;
+                    fs.writeFile(fullpath,JSON.stringify(json),(err)=>{
+                        if(err)
+                            console.log("error");
+                    });
+                })
+                response.status(200).send("completed")
+                break;
+            }
+            case "delete":{
+                console.log("delete");
+                fs.readFile(fullpath,"utf8",(err,data)=>{
+
+                    if(err) {
+                        console.log(err);
+                        return;
+                    }
+                    let json = JSON.parse(data);
+                    let index=findIndex(json,request.body.taskid);
+                    delete json[index];
+                    fs.writeFile(fullpath,JSON.stringify(json),(err)=>{
+                        if(err)
+                            console.log("error");
+                    });
+                })
+                response.status(200).send("completed")
+                break;
+            }
             default: {
                 sendFile(fullpath,response);
                 break;
@@ -45,4 +113,12 @@ function sendFile(filename,response){
         }
         response.status(200).json(JSON.parse(data));
     })
+}
+//ищет индлекс по id задачи
+function findIndex(json,id){
+    for(let key in json){
+        if(json[key].id===id){
+            return key;
+        }
+    }
 }
